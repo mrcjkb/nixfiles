8c371ce02344c7681a1c14360625aab4abe279b3
Marc Jakobi <marc.jakobi@tiko.energy>
2022-07-29 21:23:04 +0200

    Formatting

--- flake.nix
+++ flake.nix
@@ -1,11 +1,13 @@
 {
   description = "Wrapper for creating a NixOS system configuration.";
 
-  inputs.nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
-  inputs.nur.url = github:nix-community/NUR;
-  inputs.home-manager.url = github:nix-community/home-manager/release-22.05;
-  inputs.nvim-config.url = github:MrcJkb/nvim-config;
+  inputs = {
+    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    nur.url = github:nix-community/NUR;
+    home-manager.url = github:nix-community/home-manager/release-22.05;
+    nvim-config.url = github:MrcJkb/nvim-config;
+  };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, nur, home-manager, 
               nvim-config, ... }@attrs: 
