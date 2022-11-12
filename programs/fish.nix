fcb1e782b748044bfc04151945862faa7e38e608
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-12 14:41:22 +0100

    disable fish greeting

--- programs/fish.nix
+++ programs/fish.nix
@@ -2,8 +2,11 @@
   enable = true;
   useBabelfish = true;
   shellInit = ''
+    fish_vi_key_bindings
+    fish_vi_cursor
     fish-nix-shell --info-right | source
     zoxide init fish | source
+    set fish_greeting
   '';
   promptInit = ''
     starship init fish | source
