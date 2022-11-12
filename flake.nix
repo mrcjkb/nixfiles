9dfc7b64ab0ee0f23533bc13791de57ab7fb9d57
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-12 23:17:42 +0100

    replace url literals with strings

--- flake.nix
+++ flake.nix
@@ -2,16 +2,16 @@
   description = "Wrapper for creating a NixOS system configuration.";
 
   inputs = {
-    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixos-unstable;
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
-    nur.url = github:nix-community/NUR;
-    home-manager.url = github:nix-community/home-manager/release-22.05;
-    nvim-config.url = github:MrcJkb/nvim-config;
-    xmonad-session.url = github:MrcJkb/.xmonad;
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nur.url = "github:nix-community/NUR";
+    home-manager.url = "github:nix-community/home-manager/release-22.05";
+    nvim-config.url = "github:MrcJkb/nvim-config";
+    xmonad-session.url = "github:MrcJkb/.xmonad";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nur, home-manager, 
-              nvim-config, xmonad-session, ... }@attrs: 
+  outputs = { self, nixpkgs, nixpkgs-unstable, nur, home-manager,
+              nvim-config, xmonad-session, ... }@attrs:
   let
   overlay-unstable = final: prev: {
     unstable = nixpkgs-unstable.legacyPackages.${prev.system};
@@ -22,12 +22,12 @@
       specialArgs = attrs // { inherit defaultUser userEmail; };
       modules = [
         # Overlays-module makes "pkgs.unstable" available in configuration.nix
-        ({ config, pkgs, ... }: { nixpkgs.overlays = [ 
-            overlay-unstable 
+        ({ config, pkgs, ... }: { nixpkgs.overlays = [
+            overlay-unstable
             nur.overlay
           ];
         })
-        ./base.nix 
+        ./base.nix
     	  home-manager.nixosModule
         nvim-config.nixosModule
         xmonad-session.nixosModule
