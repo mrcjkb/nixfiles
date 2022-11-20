6d544f435c72108722941ef26a205bab2b5d660f
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-20 14:52:15 +0100

    fix?

--- home-manager/programs/nushell.nix
+++ home-manager/programs/nushell.nix
@@ -2,16 +2,17 @@ package: {
   inherit package;
   enable = true;
 
-  envFile = {
-    text = ''
-      mkdir -p ~/.cache/starship
-      starship init nu | save ~/.cache/starship/init.nu
-    '';
-  };
+  envFile = ''
+    mkdir -p ~/.cache/starship
+    starship init nu | save ~/.cache/starship/init.nu
+  '';
 
-  configFile = {
-    text = ''
-      source ~/.cache/starship/init.nu
-    '';
-  };
+  configFile = ''
+    let $config = {
+      filesize_metric: false
+      table_mode: rounded
+      use_ls_colors: true
+    }
+    source ~/.cache/starship/init.nu
+  '';
 }
