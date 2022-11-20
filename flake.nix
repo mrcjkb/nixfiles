d3f2709dbdecf503fe8657f496314320cafe51cf
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-20 15:08:33 +0100

    re-add home-manager-release input (commented)

--- flake.nix
+++ flake.nix
@@ -5,7 +5,8 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     nur.url = "github:nix-community/NUR";
-    home-manager.url = "github:nix-community/home-manager";
+    # home-manager.url = "github:nix-community/home-manager/release-22.05";
+    home-manager.url = "github:nix-community/home-manager/";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
     nvim-config.url = "github:MrcJkb/nvim-config";
     xmonad-session.url = "github:MrcJkb/.xmonad";
