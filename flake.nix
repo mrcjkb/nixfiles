6072c972ba260a27530edd7ac6279994c913d558
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-20 21:50:34 +0100

    add feedback

--- flake.nix
+++ flake.nix
@@ -8,10 +8,11 @@
     home-manager.url = "github:nix-community/home-manager/release-22.05";
     nvim-config.url = "github:MrcJkb/nvim-config";
     xmonad-session.url = "github:MrcJkb/.xmonad";
+    feedback.url = "github:NorfairKing/feedback";
   };
 
   outputs = { self, nixpkgs, nixpkgs-unstable, nur, home-manager,
-              nvim-config, xmonad-session, ... }@attrs:
+              nvim-config, xmonad-session, feedback, ... }@attrs:
   let
     overlay-unstable = final: prev: {
       unstable = nixpkgs-unstable.legacyPackages.${prev.system};
@@ -20,8 +21,11 @@
       nix-direnv = prev.nix-direnv.override { enableFlakes = true; };
     };
     searx = ./searx.nix;
-    mkNixosSystem = { extraModules ? [], defaultUser ? "mrcjk", userEmail ? "mrcjkb89@outlook.com" }: nixpkgs.lib.nixosSystem {
+    mkNixosSystem = { extraModules ? [], defaultUser ? "mrcjk", userEmail ? "mrcjkb89@outlook.com" }:
+    let
       system = "x86_64-linux";
+    in nixpkgs.lib.nixosSystem {
+      inherit system;
       specialArgs = attrs // { inherit defaultUser userEmail; };
       modules = [
         # Overlays-module makes "pkgs.unstable" available in configuration.nix
@@ -34,6 +38,7 @@
         ./base.nix
         home-manager.nixosModule
         nvim-config.nixosModule
+        { environment.systemPackages = [ feedback.packages.${system}.default ]; }
       ] ++ extraModules;
     };
     mkDesktopSystem = { extraModules ? [], defaultUser ? "mrcjk", userEmail ? "mrcjkb89@outlook.com" }: mkNixosSystem {
