3e5fa2c6698a889bcd2abc0779bb76017abd20fe
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-22 22:41:29 +0100

    set kitty theme

--- home-manager/programs/kitty.nix
+++ home-manager/programs/kitty.nix
@@ -8,8 +8,28 @@ package: {
   keybindings = {
     # TODO
   };
-  theme = "Material Darker";
   settings = {
     confirm_os_window_close = 0;
+    background = "#212121";
+    foreground = "#B480D6";
+    cursor = "#E6B455";
+    selection_background = "#404040";
+    selection_foreground = "#EEFFFF";
+    color0 = "#212121";
+    color1 = "#DC6068";
+    color2 = "#ABCF76";
+    color3 = "#E2795B";
+    color4 = "#6E98EB";
+    color5 = "#B480D6";
+    color6 = "#EEFFFF";
+    color7 = "#71C6E7";
+    color8 = "#424242";
+    color9 = "#F07178";
+    color10 = "#C3E88D";
+    color11 = "#C3E88D";
+    color12 = "#B0C9FF";
+    color13 = "#C792EA";
+    color14 = "#89DDFF";
+    color15 = "#d8d8d8";
   };
 }
