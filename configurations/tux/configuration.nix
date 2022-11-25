e0e732d0f26bec4e4fb220c775b1053cd3ec7703
Marc Jakobi <mrcjkb89@outlook.com>
2022-11-25 12:37:20 +0100

    Create tux configuration.nix

---
+++ configurations/tux/configuration.nix
@@ -0,0 +1,41 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  services = {
+    xserver = {
+      # Configure keymap in X11
+      layout = "us";
+      xkbVariant = "altgr-intl,";
+      # Enable touchpad support (enabled default in most desktopManager).
+      libinput.enable = true;
+    };
+  };
+
+  networking = {
+    hostName = "nixos-tux";
+  };
+
+  boot.loader.efi = {
+    canTouchEfiVariables = true;
+    efiSysMountPoint = "/boot/efi";
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
+
