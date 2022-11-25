ee5d5995f181ae1e02f9fd249bf571b948dff54c
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-25 15:05:28 +0100

    fix

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -3,7 +3,7 @@
 {
   imports =
     [
-      (modulesPath + "/profiles/qemu-guest.nix")
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
