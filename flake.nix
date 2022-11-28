0208adad7b4f5a218b5e44d1cdadcc771707c042
Marc Jakobi <mrcjkb89@outlook.com>
2022-11-28 09:48:44 +0100

    downgrade back to 22.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
     nur.url = "github:nix-community/NUR";
     home-manager.url = "github:nix-community/home-manager/release-22.05";
     nvim-config.url = "github:MrcJkb/nvim-config";
