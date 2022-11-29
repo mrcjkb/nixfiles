6ba1c71baecead4de5b454701b88c8f816eb0931
Marc Jakobi <mrcjkb89@outlook.com>
2022-11-29 18:14:53 +0100

    update to 22.11 + re-enable wezterm

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -4,7 +4,7 @@
   gh = import ./gh.nix;
   zoxide = import ./zoxide.nix pkgs.zoxide;
   alacritty = import ./alacritty.nix pkgs.alacritty;
-  # wezterm = import ./wezterm.nix pkgs.wezterm;
+  wezterm = import ./wezterm.nix pkgs.wezterm;
   nushell = import ./nushell.nix pkgs.nushell;
   direnv = import ./direnv.nix;
 }
