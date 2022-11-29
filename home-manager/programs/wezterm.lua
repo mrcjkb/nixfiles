7bf1f91448b6d9110696c050dcf1845e45d9a546
Marc Jakobi <mrcjkb89@outlook.com>
2022-11-29 17:22:24 +0100

    skip close confirmation (wezterm)

--- home-manager/programs/wezterm.lua
+++ home-manager/programs/wezterm.lua
@@ -1,7 +1,7 @@
 return {
 	font_size = 16.0,
 	window_decorations = "RESIZE",
-  window_close_confirmation = 'NeverPrompt',
+	window_close_confirmation = "NeverPrompt",
 	color_scheme = "mrcjk",
 	hide_tab_bar_if_only_one_tab = true,
 	force_reverse_video_cursor = true,
