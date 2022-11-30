6d8d9f348cf681f9af9dd0e842d2d3d0dd3f04fc
Marc Jakobi <mrcjkb89@outlook.com>
2022-11-30 13:16:04 +0100

    uncomment nushell configs

--- home-manager/programs/nushell.nix
+++ home-manager/programs/nushell.nix
@@ -2,23 +2,23 @@ package: {
   inherit package;
   enable = true;
 
-  # envFile = {
-  #   text = ''
-  #     mkdir -p ~/.cache/starship
-  #     zoxide init nushell | save ~/.zoxide.nu
-  #     starship init nu | save ~/.cache/starship/init.nu
-  #   '';
-  # };
-  #
-  # configFile = {
-  #   text = ''
-  #     let $config = {
-  #       filesize_metric: false
-  #       table_mode: rounded
-  #       use_ls_colors: true
-  #     }
-  #     source ~/.zoxide.nu
-  #     source ~/.cache/starship/init.nu
-  #   '';
-  # };
+  envFile = {
+    text = ''
+      mkdir -p ~/.cache/starship
+      zoxide init nushell | save ~/.zoxide.nu
+      starship init nu | save ~/.cache/starship/init.nu
+    '';
+  };
+
+  configFile = {
+    text = ''
+      let $config = {
+        filesize_metric: false
+        table_mode: rounded
+        use_ls_colors: true
+      }
+      source ~/.zoxide.nu
+      source ~/.cache/starship/init.nu
+    '';
+  };
 }
