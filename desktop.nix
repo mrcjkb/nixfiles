6dfe2cccb183dc7db242deed5bf379919aedfbf0
Marc Jakobi <mrcjkb89@outlook.com>
2022-12-26 12:39:24 +0100

    fix: icon environment variables

--- desktop.nix
+++ desktop.nix
@@ -1,5 +1,5 @@
 # Base module to be added to desktop systems
-{ pkgs, lib, defaultUser, userEmail, ... }:
+{ pkgs, lib, config, defaultUser, userEmail, ... }:
 {
 
   imports = [
@@ -35,56 +35,69 @@
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    unstable.pcmanfm # File browser like Nautilus, but with no Gnome dependencies
-    unstable.yubioath-flutter # Yubico Authenticator Desktop app
-    unstable.brave
-    unstable.firefox
-    unstable.joplin-desktop # Joplin (notes, desktop app)
-    unstable.simplescreenrecorder
-    unstable.inkscape-with-extensions
-    unstable.gimp
-    unstable.shutter # Screenshots
-    unstable.signal-desktop
-    unstable.gparted
-    unstable.xcolor # Color picker
-    unstable.skanlite # Lightweight sane frontend
-    unstable.xsane # Sane frontend (advanced)
-    unstable.koreader # ebook reader
-    unstable.xournalpp # notetaking software with PDF annotation support
-    texlive.combined.scheme-medium
-    biber
-    unstable.keepassxc
-    (unstable.python311.withPackages (_: [
-    ]))
-    # Haskell
-    unstable.stack
-    unstable.ghc
-    unstable.cabal-install
-    unstable.cabal2nix
-    # stack2nix # Broken
-    # unstable.haskellPackages.summoner
-    # unstable.haskellPackages.summoner-tui
-    # unstable.haskellPackages.feedback # Declarative feedback loop manager
-    unstable.hpack
-    # Rust
-    cargo
-    crate2nix
-    #
-    unstable.pandoc
-    unstable.redshift # Blue light filter
-    unstable.imagemagick
-    unstable.nitrogen # Wallpaper browser/setter for X11
-    unstable.jmtpfs # MTP (Android phone) support
-    unstable.mpv-unwrapped # Media player
-    unstable.mdp # A command-line based markdown presentation tool
-    unstable.kcat # A generic non-JVM producer and consumer for Apache Kafka
-    paperkey # Print OpenPGP or GnuPG on paper
-    unstable.asciinema # Terminal session recoreder
-    unstable.playerctl
-    unstable.gh # GitHub CLI tool
-    unstable.arduino-cli
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      unstable.pcmanfm # File browser like Nautilus, but with no Gnome dependencies
+      unstable.yubioath-flutter # Yubico Authenticator Desktop app
+      unstable.brave
+      unstable.firefox
+      unstable.joplin-desktop # Joplin (notes, desktop app)
+      unstable.simplescreenrecorder
+      unstable.inkscape-with-extensions
+      unstable.gimp
+      unstable.shutter # Screenshots
+      unstable.signal-desktop
+      unstable.gparted
+      unstable.xcolor # Color picker
+      unstable.skanlite # Lightweight sane frontend
+      unstable.xsane # Sane frontend (advanced)
+      unstable.koreader # ebook reader
+      unstable.xournalpp # notetaking software with PDF annotation support
+      texlive.combined.scheme-medium
+      biber
+      unstable.keepassxc
+      (unstable.python311.withPackages (_: [
+      ]))
+      # Haskell
+      unstable.stack
+      unstable.ghc
+      unstable.cabal-install
+      unstable.cabal2nix
+      # stack2nix # Broken
+      # unstable.haskellPackages.summoner
+      # unstable.haskellPackages.summoner-tui
+      # unstable.haskellPackages.feedback # Declarative feedback loop manager
+      unstable.hpack
+      # Rust
+      cargo
+      crate2nix
+      #
+      unstable.pandoc
+      unstable.redshift # Blue light filter
+      unstable.imagemagick
+      unstable.nitrogen # Wallpaper browser/setter for X11
+      unstable.jmtpfs # MTP (Android phone) support
+      unstable.mpv-unwrapped # Media player
+      unstable.mdp # A command-line based markdown presentation tool
+      unstable.kcat # A generic non-JVM producer and consumer for Apache Kafka
+      paperkey # Print OpenPGP or GnuPG on paper
+      unstable.asciinema # Terminal session recoreder
+      unstable.playerctl
+      unstable.gh # GitHub CLI tool
+      unstable.arduino-cli
+    ];
+    sessionVariables = {
+      # Workaround for cursor theme not being recognized
+      XCURSOR_PATH = [
+        "${config.system.path}/share/icons"
+        "$HOME/.icons"
+        "$HOME/.nix-profile/share/icons/"
+      ];
+      GTK_DATA_PREFIX = [
+        "${config.system.path}"
+      ];
+    };
+  };
 
   xdg = import ./xdg;
 
