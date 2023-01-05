af47554b4f0ff22dad1b070543494e89d8194f37
Marc Jakobi <mrcjkb89@outlook.com>
2023-01-05 19:43:06 +0100

    add cache.garnix.io

--- base.nix
+++ base.nix
@@ -19,6 +19,7 @@
       "https://mrcjkb.cachix.org"
       "https://shajra.cachix.org"
       "https://nix-community.cachix.org"
+      "https://cache.garnix.io"
     ];
   in {
     package = pkgs.nixFlakes;
@@ -43,6 +44,7 @@
         "mrcjkb.cachix.org-1:KhpstvH5GfsuEFOSyGjSTjng8oDecEds7rbrI96tjA4="
         "shajra.cachix.org-1:V0x7Wjgd/mHGk2KQwzXv8iydfIgLupbnZKLSQt5hh9o="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
   };
