d0b43b3c16d476cd7f16168bb8a20c885bf9244d
Marc Jakobi <mrcjkb89@outlook.com>
2023-03-26 17:53:59 +0200

    pin stylix rev

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     gh2rockspec.url = "github:teto/gh2rockspec";
     nurl.url = "github:nix-community/nurl";
     # stylix.url = "github:mrcjkb/stylix";
-    stylix.url = "github:danth/stylix";
+    stylix.url = "github:danth/stylix?rev=0e2f7876d2f2ae98a67d89a8bef8c49332aae5af";
     base16schemes = {
       url = "github:tinted-theming/base16-schemes";
       flake = false;
@@ -28,7 +28,6 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
     nixpkgs-unstable,
     nur,
