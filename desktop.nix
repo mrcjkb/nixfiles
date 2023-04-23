7b7184bdf9019dd9928167c4899c32f4931e4790
Marc Jakobi <marc.jakobi@tiko.energy>
2023-04-23 22:34:05 +0200

    remove cargo and stack

--- desktop.nix
+++ desktop.nix
@@ -60,63 +60,62 @@ in {
   };
 
   environment = {
-    systemPackages = with pkgs; [
-      unstable.neovide
-      unstable.pcmanfm # File browser like Nautilus, but with no Gnome dependencies
-      unstable.yubioath-flutter # Yubico Authenticator Desktop app
-      unstable.brave
-      unstable.firefox
-      unstable.joplin-desktop # Joplin (notes, desktop app)
-      unstable.simplescreenrecorder
-      unstable.inkscape-with-extensions
-      unstable.gimp
-      unstable.shutter # Screenshots
-      unstable.signal-desktop
-      unstable.gparted
-      unstable.xcolor # Color picker
-      unstable.skanlite # Lightweight sane frontend
-      unstable.xsane # Sane frontend (advanced)
-      unstable.koreader # ebook reader
-      unstable.xournalpp # notetaking software with PDF annotation support
-      texlive.combined.scheme-medium
-      biber
-      unstable.keepassxc
-      (unstable.python311.withPackages (_: [
-        ]))
-      # Nix
-      unstable.alejandra # The uncompromising nix code formatter
-      # Haskell
-      unstable.stack
-      unstable.ghc
-      unstable.cabal-install
-      unstable.cabal2nix
-      # stack2nix # Broken
-      # unstable.haskellPackages.summoner
-      # unstable.haskellPackages.summoner-tui
-      # unstable.haskellPackages.feedback # Declarative feedback loop manager
-      unstable.hpack
-      # Rust
-      cargo
-      crate2nix
-      #
-      unstable.pandoc
-      unstable.redshift # Blue light filter
-      unstable.imagemagick
-      unstable.nitrogen # Wallpaper browser/setter for X11
-      unstable.jmtpfs # MTP (Android phone) support
-      unstable.mpv-unwrapped # Media player
-      unstable.mdp # A command-line based markdown presentation tool
-      unstable.kcat # A generic non-JVM producer and consumer for Apache Kafka
-      paperkey # Print OpenPGP or GnuPG on paper
-      unstable.asciinema # Terminal session recoreder
-      unstable.ovh-ttyrec # Terminal session recoreder
-      unstable.ttygif # Convert ttyrec recordings to gif
-      unstable.playerctl
-      unstable.gh # GitHub CLI tool
-      unstable.act # Run GitHub workflows locally
-      unstable.arduino-cli
-      unstable.element-desktop # Matrix client
-      unstable.xscast # Screen cast for Xorg
+    systemPackages = with pkgs;
+      [
+        unstable.neovide
+        unstable.pcmanfm # File browser like Nautilus, but with no Gnome dependencies
+        unstable.yubioath-flutter # Yubico Authenticator Desktop app
+        unstable.brave
+        unstable.firefox
+        unstable.joplin-desktop # Joplin (notes, desktop app)
+        unstable.simplescreenrecorder
+        unstable.inkscape-with-extensions
+        unstable.gimp
+        unstable.shutter # Screenshots
+        unstable.signal-desktop
+        unstable.gparted
+        unstable.xcolor # Color picker
+        unstable.skanlite # Lightweight sane frontend
+        unstable.xsane # Sane frontend (advanced)
+        unstable.koreader # ebook reader
+        unstable.xournalpp # notetaking software with PDF annotation support
+        texlive.combined.scheme-medium
+        biber
+        unstable.keepassxc
+        (unstable.python311.withPackages (_: [
+          ]))
+        # Nix
+        unstable.alejandra # The uncompromising nix code formatter
+        # Haskell
+        unstable.ghc
+        unstable.cabal-install
+        unstable.cabal2nix
+        # stack2nix # Broken
+        # unstable.haskellPackages.summoner
+        # unstable.haskellPackages.summoner-tui
+        # unstable.haskellPackages.feedback # Declarative feedback loop manager
+        unstable.hpack
+        # Rust
+        crate2nix
+        #
+        unstable.pandoc
+        unstable.redshift # Blue light filter
+        unstable.imagemagick
+        unstable.nitrogen # Wallpaper browser/setter for X11
+        unstable.jmtpfs # MTP (Android phone) support
+        unstable.mpv-unwrapped # Media player
+        unstable.mdp # A command-line based markdown presentation tool
+        unstable.kcat # A generic non-JVM producer and consumer for Apache Kafka
+        paperkey # Print OpenPGP or GnuPG on paper
+        unstable.asciinema # Terminal session recoreder
+        unstable.ovh-ttyrec # Terminal session recoreder
+        unstable.ttygif # Convert ttyrec recordings to gif
+        unstable.playerctl
+        unstable.gh # GitHub CLI tool
+        unstable.act # Run GitHub workflows locally
+        unstable.arduino-cli
+        unstable.element-desktop # Matrix client
+        unstable.xscast # Screen cast for Xorg
       ]
       ++ (with unstable.fishPlugins; [
         fzf-fish
@@ -176,7 +175,6 @@ in {
       };
     };
   };
-
 
   xdg = import ./xdg;
 }
