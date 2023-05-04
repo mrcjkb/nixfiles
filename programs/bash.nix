54d4417cb180dfe677d7ab91f0d8e6c4f0d5ff8d
Marc Jakobi <marc.jakobi@tiko.energy>
2023-05-04 15:25:52 +0200

    disable starship for bash

--- programs/bash.nix
+++ programs/bash.nix
@@ -3,9 +3,10 @@
   interactiveShellInit = ''
     eval "$(zoxide init bash)"
   '';
-  promptInit = ''
-    eval "$(starship init bash)"
-  '';
+  # FIXME: Disabled for now because it seems buggy when in a nix shell
+  # promptInit = ''
+  #   eval "$(starship init bash)"
+  # '';
   shellAliases = {
     mkdir = "mkdir -p";
     vi = "nvim";
