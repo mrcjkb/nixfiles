95c25206c152adca86204e948b4419a76738289c
Marc Jakobi <marc.jakobi@tiko.energy>
2023-06-07 09:37:39 +0200

    enable firejail for firefox only

--- desktop-programs/firejail.nix
+++ desktop-programs/firejail.nix
@@ -1,13 +1,13 @@
 pkgs: {
-  enable = pkgs.lib.mkDefault false;
+  enable = pkgs.lib.mkDefault true;
   wrappedBinaries = {
     firefox = {
       executable = "${pkgs.lib.getBin pkgs.firefox}/bin/firefox";
       profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
     };
-    brave = {
-      executable = "${pkgs.lib.getBin pkgs.brave}/bin/brave";
-      profile = "${pkgs.firejail}/etc/firejail/brave.profile";
-    };
+    # brave = {
+    #   executable = "${pkgs.lib.getBin pkgs.brave}/bin/brave";
+    #   profile = "${pkgs.firejail}/etc/firejail/brave.profile";
+    # };
   };
 }
