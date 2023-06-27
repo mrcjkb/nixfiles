0e84aaba9d2c06d67992578d2983fe9fb7be00dc
Marc Jakobi <marc.jakobi@tiko.energy>
2023-06-27 23:24:04 +0200

    disable bash aliases, etc.

--- programs/bash.nix
+++ programs/bash.nix
@@ -1,19 +1,19 @@
 {
   enableCompletion = true;
   interactiveShellInit = ''
-    eval "$(zoxide init bash)"
-    if command -v fzf-share >/dev/null; then
-      source "$(fzf-share)/key-bindings.bash"
-      source "$(fzf-share)/completion.bash"
-    fi
+    # eval "$(zoxide init bash)"
+    # if command -v fzf-share >/dev/null; then
+    #   source "$(fzf-share)/key-bindings.bash"
+    #   source "$(fzf-share)/completion.bash"
+    # fi
   '';
   # promptInit = ''
   #   eval "$(starship init bash)"
   # '';
   shellAliases = {
-    mkdir = "mkdir -p";
-    vi = "nvim";
-    vim = "nvim";
-    nv = "neovide";
+    # mkdir = "mkdir -p";
+    # vi = "nvim";
+    # vim = "nvim";
+    # nv = "neovide";
   };
 }
