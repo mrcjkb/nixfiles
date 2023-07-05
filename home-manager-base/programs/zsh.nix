38de9157e25d03b171369e0c911410baa82ac3c3
Marc Jakobi <marc.jakobi@tiko.energy>
2023-07-06 01:31:32 +0200

    setup direnv for zsh

--- home-manager-base/programs/zsh.nix
+++ home-manager-base/programs/zsh.nix
@@ -7,6 +7,7 @@
     . ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
     eval "$(zoxide init zsh)" # Must be called at the end
     eval "$(starship init zsh)"
+    eval "$(direnv hook zsh)"
   '';
   shellAliases = import ./shellAliases.nix;
   plugins = with pkgs; [
