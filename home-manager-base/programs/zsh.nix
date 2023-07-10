60efaff12b538cb7db4f14618804c3640b0f2864
Marc Jakobi <marc.jakobi@tiko.energy>
2023-07-10 08:03:49 +0200

    zsh: use zplug to manage plugins

--- home-manager-base/programs/zsh.nix
+++ home-manager-base/programs/zsh.nix
@@ -5,12 +5,21 @@
   };
   initExtra = ''
     . ${pkgs.zsh-syntax-highlighting}/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+    source ${pkgs.zsh-nix-shell}/nix-shell.plugin.zsh
     eval "$(zoxide init zsh)" # Must be called at the end
     eval "$(starship init zsh)"
     eval "$(direnv hook zsh)"
   '';
   # TODO: Move
   shellAliases = import ../../programs/shellAliases.nix;
+  zplug = {
+    enable = true;
+    plugins = [
+      {name = "zsh-users/zsh-autosuggestions";}
+      {name = "jeffreytse/zsh-vi-mode";}
+      {name = "zsh-users/zsh-history-substring-search";}
+    ];
+  };
   # FIXME:
   # plugins = with pkgs; [
   #   zsh-vi-mode
