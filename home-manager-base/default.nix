d91bd81b9fcb37f2f481897d58bb9b0840644b6d
Marc Jakobi <marc.jakobi@tiko.energy>
2023-07-12 21:45:28 +0200

    add ranger-zoxide

--- home-manager-base/default.nix
+++ home-manager-base/default.nix
@@ -30,6 +30,15 @@
           };
           recursive = true;
         };
+        "ranger/plugins/zoxide" = {
+          source = pkgs.fetchFromGitHub {
+            owner = "jchook";
+            repo = "ranger-zoxide";
+            rev = "363df97af34c96ea873c5b13b035413f56b12ead";
+            hash = "sha256-HFCGuAektrSHF7GJsoKv1TJ/T/dwkYK1eUcw8nEjJII=";
+          };
+          recursive = true;
+        };
         joplin = {
           source = ./configs/joplin/.;
           recursive = true;
