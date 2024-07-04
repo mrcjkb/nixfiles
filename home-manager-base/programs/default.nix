c0ff2083a3aec8ba04ae964136e7678b9917e5a6
Marc Jakobi <marc.jakobi@tiko.energy>
2024-07-04 09:00:40 +0200

    tweak

--- home-manager-base/programs/default.nix
+++ home-manager-base/programs/default.nix
@@ -5,8 +5,8 @@
   ...
 }: {
   atuin = import ./atuin.nix {inherit pkgs;};
-  git = import ./git.nix {inherit pkgs userEmail;};
-  jujutsu = import ./jujutsu.nix {inherit pkgs userEmail;};
+  git = import ./git.nix {inherit userEmail;};
+  jujutsu = import ./jujutsu.nix {inherit userEmail;};
   gh = import ./gh.nix;
   zoxide = import ./zoxide.nix pkgs.zoxide;
   nushell = import ./nushell.nix {
