5c660f0f79542cf067a2d9863915865b3c55e1ca
Marc Jakobi <marc.jakobi@tiko.energy>
2024-09-18 09:22:43 +0200

    add jj l alias

--- home-manager-base/programs/jujutsu.nix
+++ home-manager-base/programs/jujutsu.nix
@@ -15,5 +15,11 @@
       key = "E35672602EEA430245D61B53071AC75AA313BFCA";
     };
     git.push-bookmark-prefix = "mj/push-";
+    ui.paginate = "never";
+    aliases = {
+      # `jj l` shows commits on the working-copy commit's (anonymous) bookmark
+      # compared to the `main` bookmark
+      l = ["log" "-r" "(main..@):: | (main..@)-"];
+    };
   };
 }
