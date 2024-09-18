bc962a4c498e711f1882804832b6eba97d6a5bc2
Marc Jakobi <marc.jakobi@tiko.energy>
2024-09-18 09:23:24 +0200

    jj: set ui.editor = nvim

--- home-manager-base/programs/jujutsu.nix
+++ home-manager-base/programs/jujutsu.nix
@@ -15,7 +15,10 @@
       key = "E35672602EEA430245D61B53071AC75AA313BFCA";
     };
     git.push-bookmark-prefix = "mj/push-";
-    ui.paginate = "never";
+    ui = {
+      paginate = "never";
+      editor = "nvim";
+    };
     aliases = {
       # `jj l` shows commits on the working-copy commit's (anonymous) bookmark
       # compared to the `main` bookmark
