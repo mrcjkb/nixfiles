9c6ad712ffea97b412c7a5094cd5d375b9603a42
Marc Jakobi <marc.jakobi@tiko.energy>
2024-12-30 08:40:38 +0100

    remove kcat

--- desktop.nix
+++ desktop.nix
@@ -136,7 +136,7 @@ in {
       nitrogen # Wallpaper browser/setter for X11
       jmtpfs # MTP (Android phone) support
       mpv-unwrapped # Media player
-      kcat # A generic non-JVM producer and consumer for Apache Kafka
+      # kcat # A generic non-JVM producer and consumer for Apache Kafka
       paperkey # Print OpenPGP or GnuPG on paper
       t-rec # Terminal screen recorder
       playerctl
