81349855769b7cda1842d31494f7443f279822d2
Marc Jakobi <marc.jakobi@tiko.energy>
2025-02-24 09:14:52 +0100

    work around swaylock module bug

--- home-manager-desktop/programs/default.nix
+++ home-manager-desktop/programs/default.nix
@@ -1,4 +1,7 @@
 {pkgs, ...}: {
   alacritty = import ./alacritty.nix pkgs.alacritty;
   wezterm = import ./wezterm.nix pkgs.wezterm;
+
+  # a buggy nixpkgs module causing infinite recursion in evaluation
+  swaylock.enable = pkgs.lib.mkForce false;
 }
