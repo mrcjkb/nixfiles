6e1ef676d751c6944c290d72b66d2be52ec86a08
Marc Jakobi <marc@jakobi.dev>
2026-07-10 11:30:35 +0200

    add some ghostty configs

--- home-manager-desktop/programs/ghostty.nix
+++ home-manager-desktop/programs/ghostty.nix
@@ -4,8 +4,29 @@ package: {
   installVimSyntax = true;
   enableBashIntegration = true;
   enableZshIntegration = true;
+  clearDefaultKeybinds = true;
   settings = {
     cursor-style = "block_hollow";
     window-decoration = "none";
+    window-show-tab-bar = "never";
+    window-save-state = "always";
+    keybind = [
+      "ctrl+b>n=next_tab"
+      "ctrl+b>p=previous_tab"
+      "ctrl+b>t=new_tab"
+      "ctrl+h=goto_split:left"
+      "ctrl+j=goto_split:bottom"
+      "ctrl+k=goto_split:top"
+      "ctrl+l=goto_split:right"
+      "ctrl+b>h=new_split:left"
+      "ctrl+b>j=new_split:down"
+      "ctrl+b>k=new_split:up"
+      "ctrl+b>l=new_split:right"
+      "ctrl+b>f=toggle_split_zoom"
+      "ctrl++=increase_font_size:1"
+      "ctrl+-=decrease_font_size:1"
+      "ctrl+/=start_search"
+      "ctrl+o>n=next_tab"
+    ];
   };
 }
